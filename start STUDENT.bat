4f07c11abb6acda948bbb47b95d2ae8d3310d1c1
Fredriech10 <fpretorius10@gmail.com>
2026-05-06 12:10:43 +0200

    created manage_tasks.html

--- start STUDENT.bat
+++ start STUDENT.bat
@@ -2,9 +2,4 @@
 title Learner Tracking
 echo %USERNAME% | findstr /I /B "MELHS" >nul && exit /b
 echo Opening Learner Tracking for %USERNAME%...
-start chrome "http://MELHS-CGM04VM5G:5000/auto_login?username=%USERNAME%"
-@echo off
-title Learner Tracking
-echo %USERNAME% | findstr /I /B "MELHS" >nul && exit /b
-echo Opening Learner Tracking for %USERNAME%...
 powershell -Command "Start-Process 'chrome' -ArgumentList 'http://MELHS-CGM04VM5G:5000/auto_login?username=%USERNAME%' -WindowStyle Minimized"

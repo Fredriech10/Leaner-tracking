2f8725ea4d36cfe2477d2365e605b5623141bfd6
Fredriech10 <56116416+Fredriech10@users.noreply.github.com>
2026-08-03 10:06:34 +0200

    start

--- start.bat
+++ start.bat
@@ -13,4 +13,4 @@ timeout /t 3 /nobreak >nul
 echo Opening Chrome minimized with auto-login for %USERNAME%...
 powershell -NoProfile -WindowStyle Hidden -Command "Start-Process 'chrome' -ArgumentList '--new-window','--start-minimized','http://MELHS-CGM04VM5G:5000/auto_login?username=%USERNAME%'"
 timeout /t 2 /nobreak >nul
-powershell -NoProfile -WindowStyle Hidden -Command "(New-Object -ComObject Shell.Application).MinimizeAll()"
+
